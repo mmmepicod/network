885247a347aec4ee1f1f7e60bb7151959120a6ec
Mehdi <mohamed-mehdi.mansour@epitech.eu>
2021-08-11 06:54:45 -0700

    h

---
+++ api-yowl/app/Http/Controllers/commentcontroller.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\comment;
+use Illuminate\Http\Request;
+
+class commentcontroller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $url = $request->query('url');
+        $query = comment::orderByDesc('created_at');
+        if ($url != null) {
+            $query = $query->where('url', '=', $url);
+        }
+        
+        return $query->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        comment::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function show(comment $comment)
+    {
+        return $comment;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, comment $comment)
+    {
+        $comment->update($request->all());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(comment $comment)
+    {
+        $comment->delete();
+    }
+}
---
+++ api-yowl/resources/views/vendor/jetstream/components/welcome.blade.php
@@ -0,0 +1,99 @@
+<div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+    <div>
+    <FONT color="green" size=7><h1>KOMMANTANTUR application</h1></FONT>
+    </div>
+
+    <div class="mt-8 text-2xl">
+        Welcome KOMMANTANTUR application!
+    </div>
+
+    <div class="mt-6 text-gray-500">
+        Vous êtes maintenant loggé, et faites partie désormais de notre communauté,
+        Veuillez prendre connaissance de nos régles de communauté en tapant <a href="#">ici</a>. Retrouvons ous dans la rubrique Start KOMMANTANTUR pour télécharger votre extension et commencer une folle aventure de commentaires.
+    </div>
+</div>
+
+<div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+                    <div class="grid grid-cols-1 md:grid-cols-2">
+                        <div class="p-6">
+                            <div class="flex items-center">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
+                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="#" class="underline text-gray-900 dark:text-white">Découvrir KOMMANTANTUR</a></div>
+                            </div>
+
+                            <div class="ml-12">
+                                <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                                     Kommantantur est un projet ambitieux, visant à rendre possible l'évaluation de chaque page internet, y compris les moins signifiantes pour les acteurs majeurs aujourd'hui en activité. Mue par l'objectivite et le respect de l'éthique du web, notre communauté ambitionne de commenter chaque url existante.
+                                     Grâce à notre discrète extension sur votre navigateur, déployez là en un clic dès que vous aurez besoin de vous exprimer sur une page. Soyez le bienvenue dans notre univers.
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
+                            <div class="flex items-center">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"></path><path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
+                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="#" class="underline text-gray-900 dark:text-white">Vidéo installation KOMMANTANTUR</a></div>
+                            </div>
+
+                            <div class="ml-12">
+                                <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                                    Vous n'êtes pas familier des extensions navigateur, et vous ne vous sentez pas le courage de lire notre Documentation, installez votre extension en moins d'une mn en suivant ce tutoriel vidéo, vous y trouverez ensuite des témoignages et astuces de nos primo-utilisateurs.
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="p-6 border-t border-gray-200 dark:border-gray-700">
+                            <div class="flex items-center">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path></svg>
+                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="#" class="underline text-gray-900 dark:text-white">Start KOMMANTANTUR</a></div>
+                            </div>
+
+                            <div class="ml-12">
+                                <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                                Pour commencer à commenter, appuyez sur le lien ci dessous pour télécharger votre extension, créer votre compte en moins d'une mn, et accédez à votre espace, voilà vous avez désormais le super pouvoir de commenter toutes les URLs du net.</div>
+                            </div>
+                        </div>
+
+                        <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
+                            <div class="flex items-center">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">Mot de l'équipe KAMMANTANTUR</div>
+                            </div>
+
+                            <div class="ml-12">
+                                <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                                    Nous avons commencé cette folle aventure, sans imaginer un jour devenir la référence que nous sommes, ceci a été possible grâce à vous, nous vous en sommes réconnaissants et vous félicitons pour votre implication dans Kommantantur. Nous vous aimons .
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
+                    <div class="text-center text-sm text-gray-500 sm:text-left">
+                        <div class="flex items-center">
+                            <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="-mt-px w-5 h-5 text-gray-400">
+                                <path d="M7.646 15.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 14.293V5.5a.5.5 0 0 0-1 0v8.793l-2.146-2.147a.5.5 0 0 0-.708.708l3 3z"></path>
+                            </svg>
+
+                            <FONT color="blue" size=4 >
+                            <a href="extension" download>
+                                Télécharger Kommantantur Extension
+                            </a>
+                            </FONT>
+
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="ml-4 -mt-px w-5 h-5 text-gray-400">
+                                <path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
+                            </svg>
+                            
+
+                            <a href="https://www.epitech.eu/fr/ecole-informatique-lyon/" class="ml-1 underline">
+                                Sponsorisé par EPITECH
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+                        Kommantantur version 2.0.1
+                    </div>
+                </div>
